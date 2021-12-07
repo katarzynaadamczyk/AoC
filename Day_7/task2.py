6a4876bb1fa7f60002c553887134845efee11539
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-12-07 10:59:06 +0100

    Done task 2 on day 7 (not checked)

---
+++ Day_7/task2.py
@@ -0,0 +1,21 @@
+def main():
+    with open('Day_7/data7.txt', 'r') as myfile:
+        data = myfile.readline().split(',')
+        for i in range(len(data)):
+            data[i] = int(data[i])
+        sorted_data = sorted(data)
+        if sorted_data[0] == sorted_data[-1]:
+            print('Result is 0')
+        else:
+            mean = sum(sorted_data) // (len(sorted_data))
+            result = 0
+            j = 1
+            for i in sorted_data:
+                for x in range(abs(mean - i)):
+                    result += j
+                    j += 1
+            print(f'Result is: {result}')           
+            
+
+if __name__ == '__main__':
+    main()
