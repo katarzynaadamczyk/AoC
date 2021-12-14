a92f9ca37f698ad08b0ec2228cac0e28e5e841eb
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-12-14 10:59:56 +0100

    Done task 1 & task 2 on day 14

--- Day_14/task1.py
+++ Day_14/task1.py
@@ -1,42 +1,50 @@
 # Katarzyna Adamczyk
-# Solution to day 14 task 1 of Advent of Code 2021
+# Solution to day 14 task 1&2 of Advent of Code 2021
 
 def pairinsertion(polymer, data):
-    tmp = polymer[0]
-    for i in range(len(polymer) - 1):
-        tmp += data[polymer[i:i+2]] + polymer[i+1]
+    tmp = {}
+    for key in polymer.keys():
+        tmp.setdefault(key[0] + data[key], 0)
+        tmp.setdefault(data[key] + key[1], 0)
+        tmp[key[0] + data[key]] += polymer[key]
+        tmp[data[key] + key[1]] += polymer[key]
     return tmp
     
-def countelements(polymer):
+def countelements(polymer, start):
     result = {}
-    for i in polymer:
-        result.setdefault(i, 0)
-        result[i] += 1
+    for key in polymer:
+        result.setdefault(key[0], 0)
+        result[key[0]] += polymer[key]
+    result[start[-1]] += 1
     result = sorted(result.values(), reverse=True)    
     return result[0] - result[-1]
 
-def solution(filename):
+def solution(filename, quantity):
     with open(filename, 'r') as myfile:
         data = {}
-        polymer = myfile.readline().strip()
-        print(polymer)
+        polymer = {} 
+        tmp = myfile.readline().strip()
+        for i in range(len(tmp) - 1):
+            polymer.setdefault(tmp[i:i+2], 0)
+            polymer[tmp[i:i+2]] += 1
         for line in myfile:
             line = line.strip()
             if line != '':
                 data[line[0:2]] = line[-1::]
         
-        for i in range(10):
+        for i in range(quantity):
             polymer = pairinsertion(polymer, data)
         
-        return countelements(polymer)
+        return countelements(polymer, tmp)
         
             
                     
         
 
 def main():
-    print(f'Result for test data is {solution("Day_14/testdata.txt")}')
-    print(f'Result for data 14 is {solution("Day_14/data14.txt")}')
+    print(f'Result for test data for task 1 is {solution("Day_14/testdata.txt", 10)}')
+    print(f'Result for data 14 for task 1 is {solution("Day_14/data14.txt", 10)}')
+    print(f'Result for data 14 for task 2 is {solution("Day_14/data14.txt", 40)}')
 
 if __name__ == '__main__':
     main()
