c1ed173726bd0ecea6393906df8414bb4d3aeef0
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-12-15 21:33:44 +0100

    not working solution for day 15 task 1

--- Day_15/task1.py
+++ Day_15/task1.py
@@ -1,19 +1,41 @@
 # Katarzyna Adamczyk
 # Solution to day 15 task 1 of Advent of Code 2021
 
+def findpath(data, x, y, result):
+    while True:
+      #  print(data[y][x], end=": ")
+      #  print(result)
+        if y < (len(data) - 1) and x < (len(data[y]) - 1):
+            result, x, y = min(findpath(data, x+1, y, result + data[y][x+1]), findpath(data, x, y+1, result + data[y+1][x]))
+            '''            if data[y+1][x] < data[y][x+1]:
+                result += data[y+1][x]
+                y = y + 1
+            elif data[y+1][x] > data[y][x+1]:
+                result += data[y][x+1]
+                x = x + 1
+            else:
+                result += data[y][x+1]
+                return min(findpath(data, x+1, y, result), findpath(data, x, y+1, result))
+        '''
+        elif y < (len(data) - 1) and x == (len(data[y]) - 1):
+            result += data[y+1][x]
+            y = y + 1
+        elif x < (len(data[y]) - 1) and y == (len(data) - 1):
+            result += data[y][x+1]
+            x = x + 1
+        if x == (len(data[y]) - 1) and y == (len(data) - 1):
+            break
+    return result, x, y
+                 
+
 def solution(filename):
     with open(filename, 'r') as myfile:
-        data = {}
-        polymer = {} 
-        tmp = myfile.readline().strip()
-        for i in range(len(tmp) - 1):
-            polymer.setdefault(tmp[i:i+2], 0)
-            polymer[tmp[i:i+2]] += 1
+        data = [] 
         for line in myfile:
-            line = line.strip()
-            if line != '':
-                data[line[0:2]] = line[-1::] 
+            tmp = [int(x) for x in line.strip()]
+            data.append(tmp)
         
+        return findpath(data, 0, 0, 0)
 
 def main():
     print(f'Result for test data for task 1 is {solution("Day_15/testdata.txt")}')
