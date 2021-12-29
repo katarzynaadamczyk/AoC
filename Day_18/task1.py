8ba4f597fdb9c34d3f794115dcd4e9d3478c3379
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-12-29 17:11:00 +0100

    Done first not working version for adding snailfish numbers

--- Day_18/task1.py
+++ Day_18/task1.py
@@ -5,21 +5,19 @@
 def checkposition(num):
     # check position of place to explode a pair or split a number
     numberofbrackets = 0
-    positionofnumber = -1
     for i in range(len(num)):
         if num[i] == '[':
             numberofbrackets += 1
             if numberofbrackets > 4:
-                break
+                return i
         elif num[i] == ']':
             numberofbrackets -= 1
         elif num[i].isdecimal():
             if i < len(num) - 1 and num[i+1].isdecimal():
-                if positionofnumber < 0:
-                    positionofnumber = i
+                return i
     
     
-    return i if i < len(num) else positionofnumber if positionofnumber > 0 else len(num)
+    return len(num)
 
 def explodeapair(num, pos):
     # explode a pair at given pos
@@ -28,7 +26,7 @@ def explodeapair(num, pos):
     rightnum = int(num[num.find(',', pos)+1:endpairpos])
     
     num = num[0:pos] + '0' + num[endpairpos+1::]
-    print(num)
+    #print(num)
     for i in range(pos-1, 0, -1):
         if num[i].isdecimal():
             for j in range(i - 1, 0, -1):
@@ -37,7 +35,7 @@ def explodeapair(num, pos):
             pos += i - j
             num = num[0:j+1] + str(leftnum + int(num[j+1:i+1])) + num[i+1::]
             break
-    print(num)
+    #print(num)
     for i in range(pos+1, len(num)):
         if num[i].isdecimal():
             for j in range(i + 1, len(num)):
@@ -45,20 +43,17 @@ def explodeapair(num, pos):
                     break
             num = num[0:i] + str(rightnum + int(num[i:j])) + num[j::]
             break    
-    print(num)
+    #print(num)
     return num
 
 def splitanumber(num, pos):
     # split a number at given pos
-    for char in ',[]':
-        commapos = num.find(char, pos)
-        if commapos >= 0:
-            break
+    numtosplit = int(num[pos:pos+2])
     pairtoadd = '['
-    pairtoadd += str(int(num[pos:commapos]) // 2) + ','
-    pairtoadd += str((int(num[pos:commapos]) + 1) // 2) + ']'
+    pairtoadd += str(numtosplit // 2) + ','
+    pairtoadd += str((numtosplit + 1) // 2) + ']'
     
-    return num[0:pos] + pairtoadd + num[commapos::]
+    return num[0:pos] + pairtoadd + num[pos+2::]
 
 def explodeorsplit(num, pos):
     # explode or split a snailfish number
@@ -70,7 +65,7 @@ def shorten(num):
     # shortening the num
     pos = checkposition(num)
     while pos < len(num) - 1:
-        print(num)
+        #print(num)
         num = explodeorsplit(num, pos)
         pos = checkposition(num)
     return num
@@ -82,7 +77,7 @@ def magnitude(num):
 def solution1(filename):
     with open(filename, 'r') as myfile:
         sum = ''
-        i = 0
+        #i = 0
         for line in myfile:    
             line = shorten(line.strip())
             if sum == '':
@@ -90,15 +85,17 @@ def solution1(filename):
             else:
                 sum = '[' + sum + ',' + line + ']'
                 sum = shorten(sum)
-                i += 1
-            if i > 2:
-                break    
-        print(sum)
+            print(sum)
+            #    i += 1
+            #if i > 2:
+            #    break    
+        #print(sum)
         return magnitude(sum)
     
 
 def main():
     print(f'Result for test data for task 1 is {solution1("Day_18/testdata.txt")}')
+    print(f'Result for test data for task 1 is {solution1("Day_18/testdata1.txt")}')
     #print(f'Result for data 18 for task 1 is {solution1("Day_18/data18.txt")}')
 
 if __name__ == '__main__':
