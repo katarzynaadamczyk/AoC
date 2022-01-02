3ba5c12ab5a5dc76b1bf21eff244f9312763605d
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-01-02 20:23:03 +0100

    Done task 1 day 19

--- Day_19/task1.py
+++ Day_19/task1.py
@@ -1,51 +1,77 @@
 # Katarzyna Adamczyk
 # Solution to day 19 task 1 of Advent of Code 2021
 
+transformation_table = [
+                        [[0, 1, 2], [1, 1, 1]],
+                        [[0, 1, 2], [-1, 1, 1]],
+                        [[0, 1, 2], [1, -1, 1]],
+                        [[0, 1, 2], [1, 1, -1]],
+                        [[0, 1, 2], [-1, -1, 1]],
+                        [[0, 1, 2], [-1, 1, -1]],
+                        [[0, 1, 2], [1, -1, -1]],
+                        [[0, 1, 2], [-1, -1, -1]],
+            
+                        [[2, 1, 0], [1, 1, 1]],
+                        [[2, 1, 0], [-1, 1, 1]],
+                        [[2, 1, 0], [1, -1, 1]],
+                        [[2, 1, 0], [1, 1, -1]],
+                        [[2, 1, 0], [-1, -1, 1]],
+                        [[2, 1, 0], [-1, 1, -1]],
+                        [[2, 1, 0], [1, -1, -1]],
+                        [[2, 1, 0], [-1, -1, -1]],
+                        
+                        [[0, 2, 1], [1, 1, 1]],
+                        [[0, 2, 1], [-1, 1, 1]],
+                        [[0, 2, 1], [1, -1, 1]],
+                        [[0, 2, 1], [1, 1, -1]],
+                        [[0, 2, 1], [-1, -1, 1]],
+                        [[0, 2, 1], [-1, 1, -1]],
+                        [[0, 2, 1], [1, -1, -1]],
+                        [[0, 2, 1], [-1, -1, -1]],
+                        
+                        [[1, 0, 2], [1, 1, 1]],
+                        [[1, 0, 2], [-1, 1, 1]],
+                        [[1, 0, 2], [1, -1, 1]],
+                        [[1, 0, 2], [1, 1, -1]],
+                        [[1, 0, 2], [-1, -1, 1]],
+                        [[1, 0, 2], [-1, 1, -1]],
+                        [[1, 0, 2], [1, -1, -1]],
+                        [[1, 0, 2], [-1, -1, -1]],
+                        
+                        [[1, 2, 0], [1, 1, 1]],
+                        [[1, 2, 0], [-1, 1, 1]],
+                        [[1, 2, 0], [1, -1, 1]],
+                        [[1, 2, 0], [1, 1, -1]],
+                        [[1, 2, 0], [-1, -1, 1]],
+                        [[1, 2, 0], [-1, 1, -1]],
+                        [[1, 2, 0], [1, -1, -1]],
+                        [[1, 2, 0], [-1, -1, -1]],
+                        
+                        [[2, 0, 1], [1, 1, 1]],
+                        [[2, 0, 1], [-1, 1, 1]],
+                        [[2, 0, 1], [1, -1, 1]],
+                        [[2, 0, 1], [1, 1, -1]],
+                        [[2, 0, 1], [-1, -1, 1]],
+                        [[2, 0, 1], [-1, 1, -1]],
+                        [[2, 0, 1], [1, -1, -1]],
+                        [[2, 0, 1], [-1, -1, -1]],
+                        ]
+
+
 def transformation(point, num):
-    if num == 0:
-        return point
-    if num == 1:
-        return [-1 * point[1], point[0], point[2]]
-    if num == 2:
-        return [-1 * point[0], -1 * point[1], point[2]]
-    if num == 3:
-        return [point[1], -1 * point[0], point[2]]
-    if num == 4:
-        return [point[0], point[1], -1 * point[2]]
-    if num == 5:
-        return [-1 * point[1], point[0], -1 * point[2]]
-    if num == 6:
-        return [-1 * point[0], -1 * point[1], -1 * point[2]]
-    if num == 7:
-        return [point[1], -1 * point[0], -1 * point[2]]
-    if num == 8:
-        return [point[2], point[1], point[0]]
-    if num == 9:
-        return [-1 * point[2], point[1], point[0]]
-    if num == 10:
-        return [-1 * point[0], point[1], -1 * point[2]]
-    if num == 11:
-        return [point[2], point[1], -1 * point[0]]
-    if num == 12:
-        return [point[0], point[2], -1 * point[1]]
-    if num == 13:
-        return [-1 * point[2], point[0], -1 * point[1]]
-    if num == 14:
-        return [-1 * point[0], -1 * point[2], -1 * point[1]]
-    if num == 15:
-        return [point[2], -1 * point[0], -1 * point[1]]
-    
+    return [point[transformation_table[num][0][x]] * transformation_table[num][1][x] for x in range(3)]
 
 def countuniquepoints(scannersdata):
     uniquepoints = set()
     for point in scannersdata[0]:
         uniquepoints.add(tuple(point))
     del scannersdata[0]
-    counti = len(scannersdata)
-    while len(scannersdata) > 0 and counti > 0:
+    changesmade = 1
+    while len(scannersdata) > 0 and changesmade > 0:
         scannerstodelete = dict() # noofscanner: [ifortransformation, [diffvector]]
+        changesmade = 0
         for scanner in range(len(scannersdata)):
-            for i in range(12):
+            for i in range(len(transformation_table)):
                 howmanydiffer = dict()
                 for point in uniquepoints:
                     for scannerpoint in scannersdata[scanner]:
@@ -59,8 +85,11 @@ def countuniquepoints(scannersdata):
                             diffvector = key
                             break
                     scannerstodelete.setdefault(scanner, [i, diffvector])
+                    changesmade += 1
                     break
         for key in sorted(list(scannerstodelete.keys()), reverse=True):
+            print(f'delete key = {key}')
+            print(f'num = {scannerstodelete[key][0]}')
             for point in scannersdata[key]:
                 # diffvector = scannerstodelete[key][1]
                 # num = scannerstodelete[key][0]
@@ -68,7 +97,7 @@ def countuniquepoints(scannersdata):
                 newpoint = [newpoint[x] + scannerstodelete[key][1][x] for x in range(3)]
                 uniquepoints.add(tuple(newpoint))
             del scannersdata[key]
-        counti -= 1
+
     print(len(scannersdata))
     return len(uniquepoints)
 
@@ -87,11 +116,30 @@ def solution1(filename):
         scannersdata.append(tmp)
         
         return countuniquepoints(scannersdata)
+    
+def solution2(filename):
+    with open(filename, 'r') as myfile:
+        scannersdata = []
+        tmp = []
+        for line in myfile:    
+            if len(line) > 1:
+                if 'scanner' in line:
+                    tmp = []
+                elif line[0].isdecimal() or line[0] == '-':
+                    tmp.append([int(x) for x in line.strip().split(',')])
+            else:
+                scannersdata.append(tmp)
+        scannersdata.append(tmp)
+        
+        return countmanhattandistance(scannersdata)
 
 
 def main():
     print(f'Result for test data for task 1 is {solution1("Day_19/testdata.txt")}')
     print(f'Result for data 19 for task 1 is {solution1("Day_19/data19.txt")}')
+    
+    print(f'Result for test data for task 2 is {solution2("Day_19/testdata.txt")}')
+    print(f'Result for data 19 for task 2 is {solution2("Day_19/data19.txt")}')
     
 
 if __name__ == '__main__':
