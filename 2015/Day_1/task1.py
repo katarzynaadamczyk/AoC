aa8755dc719329cfbab848e3e1fbb18a2c93f96e
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-02-03 10:13:39 +0100

    Done task 2 day 1 2015

--- 2015/Day_1/task1.py
+++ 2015/Day_1/task1.py
@@ -1,5 +1,5 @@
 # Katarzyna Adamczyk
-# Solution to day 25 task 1 of Advent of Code 2015
+# Solution to day 25 task 1&2 of Advent of Code 2015
 
 from math import floor
 
@@ -17,14 +17,34 @@ def countfloors(data):
             floor -= 1
     return floor
 
+def gettothebasement(data):
+    floor = 0
+    for i in range(len(data)):
+        if data[i] == '(':
+            floor += 1
+        elif data[i] == ')':
+            floor -= 1
+        if floor == -1:
+            return i+1
+    return -1
+
 def solution1(filename):
     data = getdata(filename)
         
     return countfloors(data)
+
+def solution2(filename):
+    data = getdata(filename)
+        
+    return gettothebasement(data)
     
 def main():
     print(f'Result for test data for task 1 is {solution1("2015/Day_1/testdata.txt")}')
     print(f'Result for data 25 for task 1 is {solution1("2015/Day_1/data1.txt")}')
+    
+    
+    print(f'Result for test data for task 1 is {solution2("2015/Day_1/testdata.txt")}')
+    print(f'Result for data 25 for task 1 is {solution2("2015/Day_1/data1.txt")}')
     
 
 if __name__ == '__main__':
