120d76c4dbdbbf4e9a4d23fcfce1d44984f413c0
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-12-15 23:03:06 +0100

    done task 1 day 15

--- 2022/Day_15/task_15.py
+++ 2022/Day_15/task_15.py
@@ -13,6 +13,7 @@ solution 2 -
 def manhattan_distance(point_1, point_2):
     return abs(point_1[0] - point_2[0]) + abs(point_1[1] - point_2[1])
 
+
 def get_sensors(filename):
     sensors_and_beacons, sensors_and_distance = set(), dict()
     with open(filename, 'r') as myfile:
@@ -26,41 +27,53 @@ def get_sensors(filename):
     return sensors_and_beacons, sensors_and_distance
 
 
-def get_points(sensor, point, distance):
-    if manhattan_distance((point[0] + 1, point[1]), sensor) <= distance:
-        yield (point[0] + 1, point[1])
-    if manhattan_distance((point[0] - 1, point[1]), sensor) <= distance:
-        yield (point[0] - 1, point[1])
-    if manhattan_distance((point[0], point[1] + 1), sensor) <= distance:
-        yield (point[0], point[1] + 1)
-    if manhattan_distance((point[0], point[1] - 1), sensor) <= distance:
-        yield (point[0], point[1] - 1)
+def remove_duplicates_on_x(duplicated_set):
+    new_set, lst_to_check, duplicated_items, changes_done = set(), list(duplicated_set), list(), True
+    while changes_done:
+        changes_done = False
+        item_to_check = lst_to_check[0]
+        duplicated_items.append(item_to_check)
+        for item in lst_to_check[1:]:
+            if item_to_check[0] <= item[0] <= item_to_check[1] or item_to_check[0] <= item[1] <= item_to_check[1] \
+                or item[0] <= item_to_check[0] <= item[1] or item[0] <= item_to_check[1] <= item[1]:
+                    duplicated_items.append(item)
+                    changes_done = True
+        for item in duplicated_items:
+            if item in duplicated_set:
+                duplicated_set.remove(item)
+            if item in new_set:
+                new_set.remove(item)
+        new_set.add(tuple([min(duplicated_items, key=lambda item: item[0])[0], max(duplicated_items, key=lambda item: item[1])[1]]))
+        duplicated_items = []
+        lst_to_check = list(new_set.union(duplicated_set))
+    return new_set
 
-def get_sensor_area(sensor, point, distance, area):
-    area.add(point)
-    for new_point in get_points(sensor, point, distance):
-        get_sensor_area(sensor, new_point, distance, area)
-
-
-def get_covered_area(sensors_and_distance):
-    covered_area = set()
+def solution_1(sensors_and_beacons, sensors_and_distance, row):
+    x_areas_to_check_for_duplicates = set()
     for sensor, distance in sensors_and_distance.items():
-        get_sensor_area(sensor, sensor, distance, covered_area)
-    return covered_area
+        y_dist = abs(row - sensor[1])
+        if y_dist <= distance:
+            x_dist = distance - y_dist
+            x_areas_to_check_for_duplicates.add((sensor[0] - x_dist, sensor[0] + x_dist))
+    x_areas = remove_duplicates_on_x(x_areas_to_check_for_duplicates)
+    sensors_and_beacons_in_row = list(filter(lambda point: point[1] == row, sensors_and_beacons))
+    x_sensor_and_beacons_count = 0
+    for line in x_areas:
+        for point in sensors_and_beacons_in_row:
+            if line[0] <= point[0] <= line[1]:
+                x_sensor_and_beacons_count += 1
+    return sum([point[1] - point[0] + 1 for point in x_areas]) - x_sensor_and_beacons_count
 
-def solution_1(sensors_and_beacons, covered_area, row):
-    row_covered_area = set(filter(lambda point: point[1] == row, covered_area))
-    return len(row_covered_area - sensors_and_beacons)
+    
+    
 
         
 
 def main():
     test_sensors_and_beacons, test_sensors_and_distances = get_sensors('2022/Day_15/test.txt')
-    test_covered_area = get_covered_area(test_sensors_and_distances)
-    print('test 1:', solution_1(test_sensors_and_beacons, test_covered_area, 10))
+    print('test 1:', solution_1(test_sensors_and_beacons, test_sensors_and_distances, 10))
     task_sensors_and_beacons, task_sensors_and_distances = get_sensors('2022/Day_15/task.txt')
-    task_covered_area = get_covered_area(task_sensors_and_distances)
-    print('Solution 1:', solution_1(task_sensors_and_beacons, task_covered_area, 2000000))
+    print('Solution 1:', solution_1(task_sensors_and_beacons, task_sensors_and_distances, 2000000))
  #   print('test 2:', solution_2(test_walls, sand_test_start))
   #  print('Solution 2:', solution_2(task_walls, sand_task_start))
     
