bc3ca199425a8137f4a943bd3e62c1a7b8447718
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-12-21 12:43:10 +0100

    started working on day 21

---
+++ 2022/Day_21/task_21.py
@@ -0,0 +1,51 @@
+'''
+Advent of Code 
+2022 day 21
+my solution to tasks from day 21
+
+
+solution 1 - 
+solution 2 - 
+
+'''
+from queue import Queue
+
+operations = {
+              '+': lambda x, y: x + y,
+              '-': lambda x, y: x - y,
+              '*': lambda x, y: x * y,
+              '/': lambda x, y: x // y
+              }
+
+def get_monkey_shouts(filename):
+    valves, start_valve = {}, 'AA' # name: Valve(name, pressure, set_of_next_valves)
+    with open(filename, 'r') as myfile:
+        for line in myfile:
+            line = line.strip()
+            name = line[line.find(' ') + 1:line.find(' ', line.find(' ') + 1)]
+            pressure = int(line[line.find('=')+1:line.find(';')])
+            next_valves = line[line.find(' ', line.find('valv'))+1:].split(', ')
+            valves.setdefault(name, Valve(name, next_valves, pressure))
+    return valves, start_valve
+
+
+
+
+
+def solution_1(valves, start_valve, minutes):
+    
+    return 0
+
+  
+def main():
+    test_valves, test_start_valve = get_valves('2022/Day_16/test.txt')
+    print('test 1:', solution_1(test_valves, test_start_valve, 30))
+    task_valves, task_start_valve = get_valves('2022/Day_16/task.txt')
+    print('Solution 1:', solution_1(task_valves, task_start_valve, 30))
+  #  print('test 2:', solution_2(test_valves, test_start_valve, 26))
+   # print('Solution 2:', solution_2(task_valves, task_start_valve, 26))
+    
+    
+if __name__ == '__main__':
+    main()
+    
