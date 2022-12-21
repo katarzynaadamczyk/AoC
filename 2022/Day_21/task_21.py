a68883159eae41bcad4918d486257a8572879953
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-12-21 12:57:26 +0100

    done file parsing for day 21

--- 2022/Day_21/task_21.py
+++ 2022/Day_21/task_21.py
@@ -17,31 +17,45 @@ operations = {
               '/': lambda x, y: x // y
               }
 
+values = 'values'
+operation_sign = 'operation'
+
 def get_monkey_shouts(filename):
-    valves, start_valve = {}, 'AA' # name: Valve(name, pressure, set_of_next_valves)
+    results, monkey_operations, monkey_dependance = {}, {}, {}
     with open(filename, 'r') as myfile:
         for line in myfile:
-            line = line.strip()
-            name = line[line.find(' ') + 1:line.find(' ', line.find(' ') + 1)]
-            pressure = int(line[line.find('=')+1:line.find(';')])
-            next_valves = line[line.find(' ', line.find('valv'))+1:].split(', ')
-            valves.setdefault(name, Valve(name, next_valves, pressure))
-    return valves, start_valve
+            monkey, operation = line.strip().split(': ')
+            operation = operation.split()
+            if len(operation) == 1:
+                results.setdefault(monkey, int(operation[0]))
+            else:
+                monkey_dict = {values: (operation[0], operation[2]),
+                               operation_sign: operation[1]}
+                monkey_operations.setdefault(monkey, monkey_dict)
+                monkey_dependance.setdefault(operation[0], set())
+                monkey_dependance.setdefault(operation[2], set())
+                monkey_dependance[operation[0]].add(monkey)
+                monkey_dependance[operation[2]].add(monkey)
+    print(results)
+    print(monkey_operations)
+    print(monkey_dependance)
+    
+    return results, monkey_operations, monkey_dependance
 
 
 
 
 
-def solution_1(valves, start_valve, minutes):
+def solution_1(results, monkey_operations, monkey_dependance):
     
     return 0
 
   
 def main():
-    test_valves, test_start_valve = get_valves('2022/Day_16/test.txt')
-    print('test 1:', solution_1(test_valves, test_start_valve, 30))
-    task_valves, task_start_valve = get_valves('2022/Day_16/task.txt')
-    print('Solution 1:', solution_1(task_valves, task_start_valve, 30))
+    test_results, test_monkey_operations, test_monkey_dependance = get_monkey_shouts('2022/Day_21/test.txt')
+    print('test 1:', solution_1(test_results, test_monkey_operations, test_monkey_dependance))
+    task_results, task_monkey_operations, task_monkey_dependance = get_monkey_shouts('2022/Day_21/task.txt')
+  #  print('Solution 1:', solution_1(task_results, task_monkey_operations, task_monkey_dependance))
   #  print('test 2:', solution_2(test_valves, test_start_valve, 26))
    # print('Solution 2:', solution_2(task_valves, task_start_valve, 26))
     
