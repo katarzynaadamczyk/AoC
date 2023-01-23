df4e07b21ac2615c4eceb50e991916199721e2f7
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-01-23 22:51:12 +0100

    started working on day 22

---
+++ 2022/Day_22/task_22.py
@@ -0,0 +1,32 @@
+'''
+Advent of Code 
+2022 day 22
+my solution to tasks from day 22
+
+
+solution 1 - 
+solution 2 - 
+
+'''
+
+def get_map(filename):
+    pass
+
+def get_dirs(filename):
+    pass
+
+
+def solution_1(task_map, task_dirs):
+    pass
+
+  
+def main():
+    test_map, test_dirs = get_map('2022/Day_22/test_map.txt'), get_dirs('2022/Day_22/test_dirs.txt') 
+    print('test 1:', solution_1(test_map, test_dirs))
+    task_map, task_dirs = get_map('2022/Day_22/task_map.txt'), get_dirs('2022/Day_22/task_dirs.txt') 
+    print('Solution 1:', solution_1(task_map, task_dirs))
+    
+    
+if __name__ == '__main__':
+    main()
+    
