cc67662dcafcdd4336cb2e9281ba9ee57e2fe886
Katarzyna <kasia.cholost@gmail.com>
2023-12-01 23:47:37 +0100

    added comments

--- 2023/Day_1/task_1.py
+++ 2023/Day_1/task_1.py
@@ -3,8 +3,10 @@ Advent of Code
 2023 day 1
 my solution to task 1 
 
-solution 1 - 
-solution 2 - 
+solution 1 - finds all occurences of digits in each line and forms from it a correct two-digit number, 
+adds it to a list of numbers and then generates its sum
+solution 2 - for each line searches for first and last occurence of each number, then evaluates its position 
+and at the end adds the result to list of numbers. I wanted to do as first solution, but some of words overlap and it was not enough. 
 
 '''
 import re
@@ -22,23 +24,18 @@ def solution_1(filename):
 
 def solution_2(filename):
     data = []
-    pattern = '|'.join(nums.keys())
     with open(filename, 'r') as myfile:
         for line in myfile:
             tmp = [[len(line), ''], [0, '']]
             for key in nums.keys():
                 index = line.find(key)
                 rindex = line.rfind(key)
-              #  print(index)
-             #   print(rindex)
                 if index != -1 and tmp[0][0] >= index:
                     tmp[0][0] = index
                     tmp[0][1] = key
                 if rindex != -1 and tmp[1][0] <= rindex:
                     tmp[1][0] = rindex
                     tmp[1][1] = key
-            print(line)
-            print(tmp)
             data.append(int(nums[tmp[0][1]] + nums[tmp[1][1]]))
     return sum(data)
 
