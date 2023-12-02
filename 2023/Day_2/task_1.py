ea586468f16fe672673b2602b591269004edff85
Katarzyna <kasia.cholost@gmail.com>
2023-12-02 10:17:36 +0100

    done task 2 day 2 2023

--- 2023/Day_2/task_1.py
+++ 2023/Day_2/task_1.py
@@ -7,6 +7,7 @@ solution 1 -
 solution 2 - 
 
 '''
+from functools import reduce
 
 def solution_1(filename):
     games_sum = 0
@@ -29,30 +30,29 @@ def solution_1(filename):
             if add:
                 games_sum += act_game_num
     return games_sum
-'''
+
 def solution_2(filename):
-    data = []
+    powers_sum = 0
     with open(filename, 'r') as myfile:
         for line in myfile:
-            tmp = [[len(line), ''], [0, '']]
-            for key in nums.keys():
-                index = line.find(key)
-                rindex = line.rfind(key)
-                if index != -1 and tmp[0][0] >= index:
-                    tmp[0][0] = index
-                    tmp[0][1] = key
-                if rindex != -1 and tmp[1][0] <= rindex:
-                    tmp[1][0] = rindex
-                    tmp[1][1] = key
-            data.append(int(nums[tmp[0][1]] + nums[tmp[1][1]]))
-    return sum(data)
-'''
+            line = line[line.find(':') + 1:].strip()
+            games = line.split(';')
+            act_vals = {}
+            for game in games:
+                act_game = game.split(',')
+                for color in act_game:
+                    act_color = color.strip().split()
+                    act_vals.setdefault(act_color[1], 0)
+                    act_vals[act_color[1]] = max(act_vals[act_color[1]], int(act_color[0]))
+            powers_sum += reduce(lambda a, b: a * b, act_vals.values())
+    return powers_sum
+
 def main():
     print('test 1:', solution_1('2023/Day_2/test_1.txt'))
     print('Solution 1:', solution_1('2023/Day_2/task_1.txt'))
     
-   # print('test 2:', solution_2('2023/Day_1/test_2.txt'))
-  #  print('Solution 2:', solution_2('2023/Day_1/task_1.txt'))
+    print('test 2:', solution_2('2023/Day_2/test_1.txt'))
+    print('Solution 2:', solution_2('2023/Day_2/task_1.txt'))
 
 if __name__ == '__main__':
     main()
