90bcbd38e249ef0202e55deb377d56a42092dec8
Katarzyna <kasia.cholost@gmail.com>
2023-12-05 10:14:35 +0100

    repaired code for day 4 - made a function for counting wins

--- 2023/Day_4/task.py
+++ 2023/Day_4/task.py
@@ -8,19 +8,23 @@ solution 2 -
 
 '''
 
+def get_act_count_of_winning_nums(line):
+    line = line[line.find(':') + 1:]
+    line = line.split('|')
+    winning_nums = set()
+    for num in line[0].strip().split():
+        winning_nums.add(int(num))
+    act_count = 0
+    for num in line[-1].strip().split():
+        if int(num) in winning_nums:
+            act_count += 1
+    return act_count
+
 def solution_1(filename):
     act_sum = 0
     with open(filename, 'r') as myfile:
         for line in myfile:
-            line = line[line.find(':') + 1:]
-            line = line.split('|')
-            winning_nums = set()
-            for num in line[0].strip().split():
-                winning_nums.add(int(num))
-            act_count = 0
-            for num in line[-1].strip().split():
-                if int(num) in winning_nums:
-                    act_count += 1
+            act_count = get_act_count_of_winning_nums(line)
             if act_count > 0:
                 act_sum += 2 ** (act_count - 1)
 
@@ -32,15 +36,7 @@ def solution_2(filename):
     with open(filename, 'r') as myfile:
         for line in myfile:
             act_num = int(line[line.find(' ') + 1:line.find(':')])
-            line = line[line.find(':') + 1:]
-            line = line.split('|')
-            winning_nums = set()
-            for num in line[0].strip().split():
-                winning_nums.add(int(num))
-            act_count = 0
-            for num in line[-1].strip().split():
-                if int(num) in winning_nums:
-                    act_count += 1
+            act_count = get_act_count_of_winning_nums(line)
             cards_count.setdefault(act_num, 1)
             winning_numbers.setdefault(act_num, act_count)
     for act_num, act_count in winning_numbers.items():
