989f0b346dd94065315cff877f8fb82974f4c13e
Katarzyna <kasia.cholost@gmail.com>
2023-12-08 09:26:30 +0100

    added comments for day 7 2023

--- 2023/Day_7/task_2.py
+++ 2023/Day_7/task_2.py
@@ -1,15 +1,13 @@
 '''
 Advent of Code 
 2023 day 7
-my solution to task 1 
+my solution to task  2
 
-solution 1 - create a list of hands sorted by its kind and then by the priority of each card, 
-then add to hands_dict rank of each hand, then sum the results of multiplication of bid and rank for each hand
-
+solution 2 - as solution 1, just adds the count of 'J' to max count of other cards
 '''
 from collections import Counter
 
-cards_priorities = {x: i for i, x in enumerate('AKQJT98765432')}
+cards_priorities = {x: i for i, x in enumerate('AKQJT98765432J')}
 rank = 'RANK'
 bid = 'BID'
 kind = 'KIND'
@@ -24,9 +22,18 @@ tests = {'FIVE': lambda x: len(x) == 1 and 5 in x,
 
 def get_kind(hand):
     values_of_hand = Counter(hand)
+    if 'J' in values_of_hand.keys():
+        act_card, act_max = 'A', 0
+        for card, value in values_of_hand.items():
+            if value > act_max and card != 'J':
+                act_card = card
+                act_max = value
+        values_of_hand[act_card] = values_of_hand.get(act_card, 0) + values_of_hand['J']
+        del values_of_hand['J']
     for kind, test in tests.items():
         if test(values_of_hand.values()):
             return kind
+
     return None
 
 def get_data(filename):
@@ -47,21 +54,14 @@ def insert_ranks(sorted_hands, hands_data):
     for i, hand in enumerate(sorted_hands):
         hands_data[hand][rank] = max_rank - i
 
-def solution_1(filename):
+def solution_2(filename):
     data = get_data(filename)
     insert_ranks(sort_hands(data), data)
     return sum([x[bid] * x[rank] for x in data.values()])
 
-def solution_2(filename):
-    data = get_data(filename)
-    return data
-
 def main():
-    print('test 1:', solution_1('2023/Day_7/test.txt'))
-    print('Solution 1:', solution_1('2023/Day_7/task.txt'))
+    print('test 1:', solution_2('2023/Day_7/test_2.txt'))
+    print('Solution 1:', solution_2('2023/Day_7/task.txt'))
     
-  #  print('test 2:', solution_2('2023/Day_7/test.txt'))
-  #  print('Solution 2:', solution_2('2023/Day_7/task.txt'))
-
 if __name__ == '__main__':
     main()
