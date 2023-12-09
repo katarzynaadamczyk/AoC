5e23887ad5667daa1a6cd7122272147ed2e0474e
Katarzyna <kasia.cholost@gmail.com>
2023-12-09 20:37:33 +0100

    added comments for day 9

--- 2023/Day_9/task.py
+++ 2023/Day_9/task.py
@@ -3,11 +3,13 @@ Advent of Code
 2023 day 9
 my solution to task 1 & 2
 
-solution 1 - 
+solution 1 - for each line: 1 - create a list of last number for each prediction based on given line until prediction is full of 0's. 
+2 - sum all lasts numbers to get the result.
+
+solution 2 - for each line: 1 - create a list of first number for each prediction based on given line until prediction is full of 0's.
+2 - count previous predictions, 3 - add to results last received prediction, 4 - return sum of results
 
 '''
-from re import findall
-
 class Solution:
     def __init__(self, filename) -> None:
         self.get_data(filename)
@@ -30,13 +32,17 @@ class Solution:
         results = []
         for line in self.data:
             firsts = []
+            # 1
             while line.count(0) != len(line):
                 firsts.append(line[0])
                 line = [x_1 - x_0 for x_0, x_1 in zip(line[:-1], line[1:])]
             new_first = [0]
+            # 2
             for first in firsts[::-1]:
                 new_first.append(first - new_first[-1])
+            # 3
             results.append(new_first[-1])
+        # 4
         return sum(results) 
 
 
