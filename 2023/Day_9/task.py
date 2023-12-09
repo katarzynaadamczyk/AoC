b18c6cc10168024fa924152af022a9978b390449
Katarzyna <kasia.cholost@gmail.com>
2023-12-09 11:53:57 +0100

    done task 1 & task 2 day_9_2023

--- 2023/Day_9/task.py
+++ 2023/Day_9/task.py
@@ -1,7 +1,7 @@
 '''
 Advent of Code 
 2023 day 9
-my solution to task 1 
+my solution to task 1 & 2
 
 solution 1 - 
 
@@ -9,35 +9,44 @@ solution 1 -
 from re import findall
 
 class Solution:
-    def __init__(self, filename, start_node) -> None:
-        self.get_data(filename, start_node)
+    def __init__(self, filename) -> None:
+        self.get_data(filename)
 
-    def get_data(self, filename, start_node):
-        self.root, self.moves = None, ''
-        self.nodes = {}
+    def get_data(self, filename):
+        self.data = []
         with open(filename, 'r') as myfile:
-            self.moves = myfile.readline().strip()
-            self.moves_len = len(self.moves)
-            myfile.readline()
-            line = myfile.readline()
-            while line:
-                line = myfile.readline()
-        
+            for line in myfile:
+                self.data.append([int(x) for x in line.strip().split()])        
 
-    def solution(self, end_name):
-        i = 0
-        act_node = self.root
-        while act_node.get_name() != end_name:
-            act_node = self.nodes[act_node.get_next(self.moves[i % self.moves_len])]
-            i += 1
-        return i 
+    def solution_1(self):
+        lasts = []
+        for line in self.data:
+            while line.count(0) != len(line):
+                lasts.append(line[-1])
+                line = [x_1 - x_0 for x_0, x_1 in zip(line[:-1], line[1:])]
+        return sum(lasts) 
+    
+    def solution_2(self):
+        results = []
+        for line in self.data:
+            firsts = []
+            while line.count(0) != len(line):
+                firsts.append(line[0])
+                line = [x_1 - x_0 for x_0, x_1 in zip(line[:-1], line[1:])]
+            new_first = [0]
+            for first in firsts[::-1]:
+                new_first.append(first - new_first[-1])
+            results.append(new_first[-1])
+        return sum(results) 
 
 
 def main():
-    sol = Solution('2023/Day_9/test.txt', 'AAA')
-    print('test 1:', sol.solution('ZZZ'))
-    sol = Solution('2023/Day_9/task.txt', 'AAA')
-    print('Solution 1:', sol.solution('ZZZ'))
+    sol = Solution('2023/Day_9/test.txt')
+    print('test 1:', sol.solution_1())
+    print('test 2:', sol.solution_2())
+    sol = Solution('2023/Day_9/task.txt')
+    print('Solution 1:', sol.solution_1())
+    print('Solution 2:', sol.solution_2())
 
 
 if __name__ == '__main__':
