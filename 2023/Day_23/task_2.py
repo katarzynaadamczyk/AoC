d7a33014babd26a2c8d23c9a01a21b25e2b6db30
Katarzyna <kasia.cholost@gmail.com>
2024-01-29 23:15:45 +0100

    done task 2 day 23

--- 2023/Day_23/task_2.py
+++ 2023/Day_23/task_2.py
@@ -9,7 +9,7 @@ solution 2 -
 
 '''
 from copy import copy
-from queue import PriorityQueue
+from queue import PriorityQueue, Queue
 
 class Solution:
     dot = '.'
@@ -51,6 +51,48 @@ class Solution:
                 if new_point not in visited_points and self.get_point_value(new_point) in Solution.possible_moves:
                     yield new_point
 
+    def get_next_moves_3(self, point, last_point):
+        if self.get_point_value(point) in Solution.possible_moves:
+            for new_point in [(point[0] - 1, point[1]), (point[0] + 1, point[1]), (point[0], point[1] - 1), (point[0], point[1] + 1)]:
+                if new_point != last_point and self.get_point_value(new_point) in Solution.possible_moves:
+                    yield new_point
+    
+
+    def shorten_hike_map_2(self):
+        self.short_hike_map_2 = dict()
+        hike_queue = Queue()
+        visited_points = set()
+        visited_points.add(self.start_point)
+        hike_queue.put((self.start_point, self.start_point))
+        while not hike_queue.empty():
+            act_point, new_point = hike_queue.get()
+            if act_point == self.end_point or new_point == self.end_point:
+                continue
+            self.short_hike_map_2.setdefault(act_point, dict())
+            steps = 0 if act_point == new_point else 1
+            next_points = list(self.get_next_moves_3(new_point, act_point))
+            while len(next_points) == 1 and next_points[0] != self.end_point:
+                steps += 1
+                visited_points.add(next_points[0])
+                last_point = next_points[0]
+                next_points = list(self.get_next_moves_3(last_point, new_point))
+                new_point = last_point
+            if len(next_points) == 1:
+                self.short_hike_map_2.setdefault(next_points[0], dict())
+                self.short_hike_map_2[act_point].setdefault(next_points[0], steps + 1)
+                self.short_hike_map_2[next_points[0]].setdefault(act_point, steps + 1)
+            else:
+                self.short_hike_map_2.setdefault(last_point, dict())
+                self.short_hike_map_2[act_point].setdefault(last_point, steps)
+                self.short_hike_map_2[last_point].setdefault(act_point, steps)
+                for point in next_points:
+                    if point not in visited_points:
+                        visited_points.add(point)
+                        hike_queue.put((last_point, point))
+
+
+
+
 
     def solution_1(self):
         visited_points = set()
@@ -73,29 +115,25 @@ class Solution:
         return steps
     
     def solution_2(self):
+        self.shorten_hike_map_2()
+        hikes_min = 0
+        hikes_queue = PriorityQueue()
         visited_points = set()
         visited_points.add(self.start_point)
-        visited_points_max_vals = dict()
-        points_queue = PriorityQueue()
-        points_queue.put((self.start_point, 0, visited_points))
-        hikes_len = []
-        while not points_queue.empty():
-            act_point, steps, visited_points = points_queue.get()
-            print(act_point)
+        hikes_queue.put((0, self.start_point, visited_points))
+        while not hikes_queue.empty():
+            act_step, act_point, visited_points = hikes_queue.get()
+         #   print(act_point)
             if act_point == self.end_point:
-                hikes_len.append(-1 * steps)
-                continue
-            visited_points_max_vals.setdefault(act_point, steps)
-           # if steps > visited_points_max_vals[act_point]:
-           #     continue
-            visited_points_max_vals[act_point] = min(visited_points_max_vals[act_point], steps)
-            for new_point in self.get_next_moves_2(act_point, visited_points):
-                new_visited_points = copy(visited_points)
-                new_visited_points.add(new_point)
-                points_queue.put((new_point, steps - 1, new_visited_points))
-        print(visited_points_max_vals)
-        print(hikes_len)
-        return max(hikes_len)
+                hikes_min = min(hikes_min, act_step)
+                print(hikes_min)
+            for point, steps in self.short_hike_map_2[act_point].items():
+                if point not in visited_points:
+                    new_visited_points = copy(visited_points)
+                    new_visited_points.add(point)
+                    hikes_queue.put((act_step - steps, point, new_visited_points))
+        
+        return -1 * hikes_min
         
 
 
@@ -109,7 +147,7 @@ def main():
     sol = Solution('2023/Day_23/task.txt')
     print('SOLUTION')
     print('Solution 1:', sol.solution_1())
-  #  print('Solution 2:', sol.solution_2())
+    print('Solution 2:', sol.solution_2())
 
 
 if __name__ == '__main__':
