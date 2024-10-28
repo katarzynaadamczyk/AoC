e4b45577f1bb1a4cd78699f317647c03ea35892a
Katarzyna <kasia.cholost@gmail.com>
2024-10-28 14:43:14 +0100

    done parsing data for day 9 2015

--- 2015/Day_9/task.py
+++ 2015/Day_9/task.py
@@ -6,17 +6,25 @@ task 1 -
 
 '''
 
+import re
+
 
 class Solution:
 
 
     def __init__(self, filename) -> None:
+        self.graph = {}
         self.get_data(filename)
 
-    def get_data_replace(self, filename):
+    def get_data(self, filename):
         with open(filename, 'r') as myfile:
             for line in myfile:
-                pass
+                line = re.findall(r'\w+', line)
+                self.graph.setdefault(line[0], {})
+                self.graph.setdefault(line[-2], {})
+                self.graph[line[0]][line[-2]] = int(line[-1])
+                self.graph[line[-2]][line[0]] = int(line[-1])
+        print(self.graph)
 
     def solution_1(self):
         return 
@@ -29,9 +37,9 @@ def main():
     print('TEST 1')
     print('test 1:', sol.solution_1())
   #  print('test 1:', sol.solution_2())
-    sol = Solution('2015/Day_9/task.txt')
-    print('SOLUTION')
-    print('Solution 1:', sol.solution_1())
+  #  sol = Solution('2015/Day_9/task.txt')
+  #  print('SOLUTION')
+  #  print('Solution 1:', sol.solution_1())
    # print('Solution 2:', sol.solution_2())
 
 
