e73693bc23391dbabe07de9de8a43ba12ecd6cb8
Katarzyna <kasia.cholost@gmail.com>
2024-10-28 11:01:08 +0100

    done task 2 day 8 2015

--- 2015/Day_8/task.py
+++ 2015/Day_8/task.py
@@ -2,62 +2,45 @@
 Advent of Code 
 2015 day 8
 my solution to task 1
-1364 - too high
-1344 - too low
+task 1 - replace all matches by letters - A for \ \, B for \ ", C for \ x h h, counting line lenght before and after replacement to get the result
+task 2 - make counter of all characters for all lines, for each \ or " add additional \ + 2 * number of all lines 
+(as first and last quote get two additional characters)
 
 '''
+
 import re
+from collections import Counter
 
 class Solution:
-    possible_chars = {re.escape('\\') + re.escape('\\'): 1, 
-                      re.escape('\\') + r'"': 1, 
-                      re.escape('\\') + r'x[0-9a-f][0-9a-f]': 3}
 
     possible_chars_replace = {re.escape('\\') + re.escape('\\'): 'A', 
                               re.escape('\\') + r'"': 'B', 
                               re.escape('\\') + r'x[0-9a-f][0-9a-f]': 'C'}
 
     def __init__(self, filename) -> None:
+        self.chars_counter = Counter()
+        self.total_lenght = 0
+        self.total_lines = 0
+        self.result_1 = 0
         self.get_data_replace(filename)
 
     def get_data_replace(self, filename):
-        self.result = 0
         with open(filename, 'r') as myfile:
             for line in myfile:
                 line = line.strip()
-                self.result += len(line)
-                line = line.strip('"')
-                print(self.result)
-                print(line)
+                self.total_lines += 1
+                self.total_lenght += len(line)
+                self.chars_counter += Counter(line)
+                line = line[1:-1]
                 for char, repl in Solution.possible_chars_replace.items():
                     line = re.sub(char, repl, line)
-                   ## i = line.find(char)
-                   # while i > 0:
-                   #     line = line.replace(char, repl)
-                   #     i = line.find(char)
-                print(line)
-                self.result -= len(line)
-                print(self.result)
-
-
-    # not working for my data, working for test data
-    def get_data_2(self, filename):
-        self.result = 0
-        with open(filename, 'r') as myfile:
-            for line in myfile:
-                line = line.strip()
-                self.result += 2
-                line = line.strip('"')
-                line_res = 0
-                for char, val in Solution.possible_chars.items():
-                    line_res += len(re.findall(char, line))
-                    self.result += (len(re.findall(char, line)) * val)
-                
-                print(line, line_res)
-
+                self.result_1 -= len(line)
 
     def solution_1(self):
-        return self.result
+        return self.total_lenght + self.result_1
+    
+    def solution_2(self):
+        return self.total_lines * 2 + self.chars_counter['\\'] + self.chars_counter['"']
     
 
 
@@ -66,9 +49,11 @@ def main():
     sol = Solution('2015/Day_8/test.txt')
     print('TEST 1')
     print('test 1:', sol.solution_1())
+    print('test 1:', sol.solution_2())
     sol = Solution('2015/Day_8/task.txt')
     print('SOLUTION')
     print('Solution 1:', sol.solution_1())
+    print('Solution 2:', sol.solution_2())
 
 
 if __name__ == '__main__':
