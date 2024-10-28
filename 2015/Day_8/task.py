0b1437f42cdcfb17a05e83cf204ea198117cf34a
Katarzyna <kasia.cholost@gmail.com>
2024-10-28 10:23:03 +0100

    done task 1 day 8 2015

--- 2015/Day_8/task.py
+++ 2015/Day_8/task.py
@@ -2,24 +2,59 @@
 Advent of Code 
 2015 day 8
 my solution to task 1
-
+1364 - too high
+1344 - too low
 
 '''
 import re
 
 class Solution:
-    possible_chars = (r'\\', r'\"', r'\x\w\w')
-    def __init__(self, filename) -> None:
-        self.get_data(filename)
+    possible_chars = {re.escape('\\') + re.escape('\\'): 1, 
+                      re.escape('\\') + r'"': 1, 
+                      re.escape('\\') + r'x[0-9a-f][0-9a-f]': 3}
 
-    def get_data(self, filename):
+    possible_chars_replace = {re.escape('\\') + re.escape('\\'): 'A', 
+                              re.escape('\\') + r'"': 'B', 
+                              re.escape('\\') + r'x[0-9a-f][0-9a-f]': 'C'}
+
+    def __init__(self, filename) -> None:
+        self.get_data_replace(filename)
+
+    def get_data_replace(self, filename):
         self.result = 0
         with open(filename, 'r') as myfile:
             for line in myfile:
                 line = line.strip()
                 self.result += len(line)
-                line.strip('"')
+                line = line.strip('"')
+                print(self.result)
+                print(line)
+                for char, repl in Solution.possible_chars_replace.items():
+                    line = re.sub(char, repl, line)
+                   ## i = line.find(char)
+                   # while i > 0:
+                   #     line = line.replace(char, repl)
+                   #     i = line.find(char)
+                print(line)
+                self.result -= len(line)
+                print(self.result)
+
+
+    # not working for my data, working for test data
+    def get_data_2(self, filename):
+        self.result = 0
+        with open(filename, 'r') as myfile:
+            for line in myfile:
+                line = line.strip()
+                self.result += 2
+                line = line.strip('"')
+                line_res = 0
+                for char, val in Solution.possible_chars.items():
+                    line_res += len(re.findall(char, line))
+                    self.result += (len(re.findall(char, line)) * val)
                 
+                print(line, line_res)
+
 
     def solution_1(self):
         return self.result
