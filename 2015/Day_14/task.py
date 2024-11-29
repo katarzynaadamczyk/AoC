8b253087536983f56faa6fe7f8e8ea7d7c726286
Katarzyna <kasia.cholost@gmail.com>
2024-11-29 10:12:44 +0100

    done task 1 & 2 day 14 2015

--- 2015/Day_14/task.py
+++ 2015/Day_14/task.py
@@ -2,8 +2,8 @@
 Advent of Code 
 2015 day 14
 my solution to task 1
-task 1 - using heapq module (priority queue)
-task 2 - same
+task 1 - using math calculations
+task 2 - using results of task 1 for each minute
 
 
 '''
@@ -35,8 +35,18 @@ class Solution:
             else:
                 result += how_long % (self.reindeer_dict[reindeer]['for'] + self.reindeer_dict[reindeer]['rest']) * self.reindeer_dict[reindeer]['fly']
             reindeer_results.setdefault(reindeer, result)
+        max_result = max(reindeer_results.values())
+        return {name: value for name, value in reindeer_results.items() if value == max_result}
+    
+    def solution_2(self, how_long):
+        reindeer_results = {name: 0 for name in self.reindeer_dict.keys()}
+        for minute in range(1, how_long + 1):
+            max_reindeer = self.solution_1(minute)
+            for name in max_reindeer.keys():
+                reindeer_results[name] += 1
         print(reindeer_results)
         return max(reindeer_results.items(), key=lambda item: item[1])
+
     
 
 
@@ -45,11 +55,11 @@ def main():
     sol = Solution('2015/Day_14/test.txt')
     print('TEST 1')
     print('test 1:', sol.solution_1(1000), 'should equal Comet')
-  #  print('test 1:', sol.solution_2())
+    print('test 1:', sol.solution_2(1000))
     sol = Solution('2015/Day_14/task.txt')
     print('SOLUTION')
     print('Solution 1:', sol.solution_1(2503))
-   # print('Solution 2:', sol.solution_2())
+    print('Solution 2:', sol.solution_2(2503))
    
 
 
