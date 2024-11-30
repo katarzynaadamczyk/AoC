0a03779be1d3adf0f7c9d71bd3915422c1c5ba64
Katarzyna <kasia.cholost@gmail.com>
2024-11-30 15:20:33 +0100

    done part 2 day 16 2015

--- 2015/Day_16/task.py
+++ 2015/Day_16/task.py
@@ -2,7 +2,8 @@
 Advent of Code 
 2015 day 16
 my solution to task 1
-task 1 - 
+task 1 - iterate over Sues data to get the right one
+task 2 - add lambda functions to check for sue
 
 
 '''
@@ -41,6 +42,14 @@ class Solution:
                 return False
         return True
 
+    def check_sue_2(self, sue, check_functions):
+        for key, value in sue.items():
+            if key in check_functions.keys():
+                if check_functions[key](value, self.original_sue[key]):
+                    return False
+            elif value != self.original_sue[key]:
+                return False
+        return True
     
 
     def solution_1(self) -> int:
@@ -49,7 +58,17 @@ class Solution:
                 return key
         
         return 0
-
+    
+    def solution_2(self) -> int:
+        check_functions = {'cats': lambda value, original: value <= original,
+                           'trees': lambda value, original: value <= original,
+                           'pomeranians': lambda value, original: value >= original,
+                           'goldfish': lambda value, original: value >= original}
+        for key, sue in self.sues_dict.items():
+            if self.check_sue_2(sue, check_functions):
+                return key
+        
+        return 0
     
     
 
@@ -58,13 +77,9 @@ def main():
 
     print('TASK 1')
     sol = Solution('2015/Day_16/original_aunt.txt', '2015/Day_16/task.txt')
-   # print('TEST 1')
-   # print('test 1:', sol.solution_1(), 'should equal 62842880')
-  #  print('test 1:', sol.solution_2())
-  #  sol = Solution('2015/Day_15/task.txt')
     print('SOLUTION')
     print('Solution 1:', sol.solution_1())
-  #  print('Solution 2:', sol.solution_2())
+    print('Solution 2:', sol.solution_2())
    
 
 
