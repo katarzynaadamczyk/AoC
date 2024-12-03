b9b4ec7052f87ff93f5c9ef0cc6f49849a57d183
Katarzyna <kasia.cholost@gmail.com>
2024-12-03 21:07:15 +0100

    done part 2 day 3 2024

--- 2024/Day_3/task.py
+++ 2024/Day_3/task.py
@@ -1,10 +1,11 @@
 '''
 Advent of Code 
-2024 day 2
+2024 day 3
 my solution to tasks
-task 1 - create a table of report[i+ 1] - report[i], count min and max value and check if these values are in (1, 3) or (-3, -1) scope,
-if so report is valid
-task 2 - remove one level from report and perform check as in task 1, if it passes, report is valid
+task 1 - find all valid chunks of code using re.findall method, then in each chunk find numbers and multiply them
+task 2 - use a flag to keep the information if we should add result of next mul or not - 
+important: even though the task file is in few line we must keep the flag continously between lines 
+that is why it is not reset in each iteration 
 
 
 '''
@@ -14,22 +15,44 @@ from functools import reduce
 class Solution:
 
     def __init__(self, filename) -> None:
+        self.mul_commands = []
         self.commands = []
         self.get_data(filename)
 
     def get_data(self, filename):
         with open(filename, 'r') as myfile:
             for line in myfile:
-                self.commands.append(re.findall(r'mul\(\d{1,3},\d{1,3}\)', line.strip())) # (\d{1-3},\d{1-3})'
+                self.commands.append(line.strip())
+                self.mul_commands.append(re.findall(r'mul\(\d{1,3},\d{1,3}\)', line.strip())) 
+   
+    def get_chunk_result(self, chunk):
+        return reduce(lambda a, b: a * b, [int(x) for x in re.findall(r'\d{1,3}', chunk)])
 
+    def get_line_result(self, chunks):
+        return sum([self.get_chunk_result(chunk) for chunk in chunks])
 
     def solution_1(self) -> int:
         result = 0
-        for line in self.commands:
-            for chunk in line:
-                result += reduce(lambda a, b: a * b, [int(x) for x in re.findall(r'\d{1,3}', chunk)])
+        for line in self.mul_commands:
+            result += self.get_line_result(line)
         return result
+
     
+    def solution_2(self) -> int:
+        result = 0
+        flag = True
+        for line in self.commands:
+            for chunk in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line):
+                if chunk == 'do()':
+                    flag = True
+                elif chunk == "don't()":
+                    flag = False
+                else:
+                    if flag:
+                        result += self.get_chunk_result(chunk)
+
+            
+        return result
     
     
     
@@ -40,11 +63,11 @@ def main():
     sol = Solution('2024/Day_3/test.txt')
     print('TEST 1')
     print('test 1:', sol.solution_1(), 'should equal 161')
-  #  print('test 1:', sol.solution_2(), 'should equal 4')
+    print('test 1:', sol.solution_2(), 'should equal 48')
     sol = Solution('2024/Day_3/task.txt')
     print('SOLUTION')
     print('Solution 1:', sol.solution_1())
-  #  print('Solution 2:', sol.solution_2())
+    print('Solution 2:', sol.solution_2()) 
    
 
 
