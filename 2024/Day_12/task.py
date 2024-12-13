691ca7f9f7a2dc4880c58a375977dcc02f8315be
Katarzyna <kasia.cholost@gmail.com>
2024-12-13 11:06:45 +0100

    updated day 12 -> reduced lines of unnecessary code (things that were counted twice)

--- 2024/Day_12/task.py
+++ 2024/Day_12/task.py
@@ -14,16 +14,14 @@ task 1 - for each garden area:
 task 2 - calculating sides is a little more complicated task:
         for each garden area:
             calculate sides:
-                firstly create set of all points that not all neighboring points are in garden points set
-                then put them into correct sets: for up side, down side, left side and right side
+                then put all points that not all neighboring points are in garden points set into correct sets: 
+                    for up side, down side, left side and right side
                 for each side set calculate number of sides (side is if points on same x or y are in growing by 1 sequence, otherwise 
                 there is next side)
             
                 
 
 '''
-
-from tqdm import tqdm
 
 class Solution:
 
@@ -36,10 +34,10 @@ class Solution:
                          'left': (0, -1),
                          'right': (0, 1)}
         
-        self.num_of_sides = {'up': self.get_number_of_up_down_sides, 
-                             'down': self.get_number_of_up_down_sides,
-                             'left': self.get_number_of_left_right_sides,
-                             'right': self.get_number_of_left_right_sides}
+        self.num_of_sides = {'up': (1, 0), 
+                             'down': (1, 0),
+                             'left': (0, 1),
+                             'right': (0, 1)}
         
         self.areas = self.get_garden_areas()
 
@@ -70,7 +68,6 @@ class Solution:
                 new_new_points = new_new_points.union(self.get_possible_points(point))
             new_points = new_new_points.difference(garden_area)
         return garden_area
-
     
     def get_garden_areas(self):
         garden_areas = []
@@ -96,18 +93,18 @@ class Solution:
                     perimeter += 1
         return perimeter
 
-    def get_up_down_left_right_sides(self, sides, garden):
+    def get_up_down_left_right_sides(self, garden): 
         new_sides = {key: set() for key in self.dir_dict.keys()}
-        for point in sides:
+        for point in garden:
             for key, direction in self.dir_dict.items():
                 if (point[0] + direction[0], point[1] + direction[1]) not in garden:
                     new_sides[key].add(point)
         return new_sides
-    
-    def get_number_of_left_right_sides(self, sides):
+
+    def get_number_of_garden_sides(self, sides, indices):
         num = 0
-        for x in set([a[1] for a in sides]):
-            y = sorted([a[0] for a in sides if a[1] == x])
+        for x in set([a[indices[1]] for a in sides]):
+            y = sorted([a[indices[0]] for a in sides if a[indices[1]] == x])
             if len(y) == 1:
                 num += 1
                 continue
@@ -115,37 +112,17 @@ class Solution:
             num += 1 + len([a for a in diffs if a != 1])
         return num
 
-    def get_number_of_up_down_sides(self, sides):
-        num = 0
-        for y in set([a[0] for a in sides]):
-            x = sorted([a[1] for a in sides if a[0] == y])
-            if len(x) == 1:
-                num += 1
-                continue
-            diffs = [x2 - x1 for x1, x2 in zip(x[:-1], x[1:])]
-            num += 1 + len([a for a in diffs if a != 1])
-        return num
-
-
     def get_garden_sides(self, garden):
-        sides = set()
-        for point in garden:
-            for dir in self.dir_dict.values():
-                new_point = (point[0] + dir[0], point[1] + dir[1])
-                if new_point not in garden:
-                    sides.add(point)
-        sides = self.get_up_down_left_right_sides(sides, garden)
+        sides = self.get_up_down_left_right_sides(garden) 
         result = 0
         for direction, points in sides.items():
-            result += self.num_of_sides[direction](points)
-        
+            result += self.get_number_of_garden_sides(points, self.num_of_sides[direction])
         return result
 
     
     def solution_1(self) -> int:
         return sum([self.get_garden_area(garden) * self.get_garden_perimeter(garden) for garden in self.areas])
     
-
     
     def solution_2(self) -> int:
         return sum([self.get_garden_area(garden) * self.get_garden_sides(garden) for garden in self.areas])
@@ -155,7 +132,6 @@ class Solution:
     
 
 def main():
-
     print('TASK 1')
     sol = Solution('2024/Day_12/test.txt')
     area = sol.get_new_garden_area((0, 0))
@@ -169,6 +145,6 @@ def main():
     print('Solution 2:', sol.solution_2())
    
 
-
 if __name__ == '__main__':
     main()
+
