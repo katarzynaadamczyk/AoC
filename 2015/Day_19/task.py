757b3d6b5c5d513769548ceb6bd5a93306cb1262
Katarzyna <kasia.cholost@gmail.com>
2025-10-26 21:30:59 +0100

    2015 Day 19: working on solution 2

--- 2015/Day_19/task.py
+++ 2015/Day_19/task.py
@@ -3,12 +3,13 @@ Advent of Code
 2015 day 18
 my solution to tasks
 
-task 1 - brute force on tuples
-
+task 1 - brute force on lists & tuples (for adding to set and counting the number of distinct tuples)
+task 2 - using memoization (seen_molecules) and heapq as priority queue (instead of PriorityQueue from queue module) 
 
 '''
 import time
 from collections import defaultdict
+import heapq
 
 def time_it(func):
     def wrapper(*args,**kwargs):
@@ -64,6 +65,7 @@ class Solution:
         self.possible_changes = new_possible_changes_dict
 
     def parse_molecule(self, line: str) -> list[str]:
+        """ change string with complicated molecule into list of strings for single molecules """
         line = line.strip()
         result = []
         while line:
@@ -91,21 +93,59 @@ class Solution:
         get result for task 1
         '''
         result = set()
-        print(self.possible_molecules)
-        print(self.possible_changes)
         for i, molecule in enumerate(self.molecule):
             for values in self.possible_changes[molecule]:
                 result.add(tuple(self.molecule[:i] + values + self.molecule[i+1:]))
         return len(result)
+    
+    def check_compatibility(self, new_molecules: list[str], common_number: int = 0) -> int:
+        result = common_number
+        for a, b in zip(new_molecules, self.molecule):
+            if a != b:
+                break
+            result += 1
+        return result
 
 
     @time_it
-    def solution_2(self) -> int:
+    def solution_2(self, init_molecule: str = 'e') -> int:
         '''
         get result for task 2
         '''
-        result = 0
-        return result
+        # prepare heap
+        queue = []
+        seen_molecules = set()
+        for molecule in self.possible_changes.get(init_molecule, []):
+            common_number = self.check_compatibility(molecule)
+            heapq.heappush(queue, (1, -1 * common_number, molecule))
+            seen_molecules.add(tuple(molecule))
+        # get len of desired molecule
+        final_len = len(self.molecule)
+        # run queue
+        index = 0
+        while queue:
+            result, common_number, molecules = heapq.heappop(queue)
+            print(common_number, result, molecules)
+            common_number *= -1
+            if len(molecules) > final_len:
+                continue
+            if common_number == final_len:
+                return result
+            for i, molecule in enumerate(molecules[common_number-1], start=common_number-1):
+                for values in self.possible_changes[molecule]:
+                    new_molecules = molecules[:i] + values + molecules[i+1:]
+                    new_molecules_tuple = tuple(new_molecules)
+                    if new_molecules_tuple in seen_molecules:
+                        continue
+                    seen_molecules.add(new_molecules_tuple)
+                    common_number = self.check_compatibility(new_molecules)
+                    heapq.heappush(queue, (result + 1, -1 * common_number,  new_molecules))
+            index += 1
+            if index > 100:
+                break
+        print(len(seen_molecules))
+        print(len(queue))
+        return -1
 
 
 def main():
@@ -113,12 +153,15 @@ def main():
     sol = Solution('2015/Day_19/test.txt')
     print('TEST 1')
     print('test 1:', sol.solution_1(), 'should equal 4')
-   # print('test 2:', sol.solution_2(5), 'should equal 17')
+    print('TEST 2')
+    sol = Solution('2015/Day_19/test_2.txt')
+    print('TEST 2')
+    print('test 2:', sol.solution_2(), 'should equal 6')
     print('SOLUTION')
     sol = Solution('2015/Day_19/task.txt')
     print('SOLUTION')
     print('Solution 1:', sol.solution_1())
-  #  print('Solution 2:', sol.solution_2(100))
+    print('Solution 2:', sol.solution_2())
    
 
 
