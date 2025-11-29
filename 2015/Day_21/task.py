1b9a08241d21ca7b0b81801851fb2a02edff3c7c
Katarzyna <kasia.cholost@gmail.com>
2025-11-29 20:58:40 +0100

    Day 21: done task 1 & 2

--- 2015/Day_21/task.py
+++ 2015/Day_21/task.py
@@ -3,7 +3,7 @@ Advent of Code
 2015 day 21
 my solution to tasks
 
-task 1 - using class for players
+task 1&2 - using class for players and a bruteforce approach - checking each combination and finding min/max value afterwards
 
 
 '''
@@ -33,9 +33,9 @@ class Player:
 
 armor_gains_vs_costs = {1: 13, 2: 31, 3: 53, 4: 75, 5: 102}
 damage_gains_vs_costs = {4: 8, 5: 10, 6: 25, 7: 40, 8: 74}
-Ring = namedtuple('Ring', ['damage', 'armor'])
-rings = {Ring(1, 0): 25, Ring(2, 0): 50, Ring(3, 0): 100, 
-         Ring(0, 1): 20, Ring(0, 2): 40, Ring(0, 3): 80}
+Ring = namedtuple('Ring', ['damage', 'armor', 'cost'])
+rings = (Ring(1, 0, 25), Ring(2, 0, 50), Ring(3, 0, 100), 
+         Ring(0, 1, 20), Ring(0, 2, 40), Ring(0, 3, 80))
 
 class Solution:
     def __init__(self, filename: str, initial_player_hitpoints: int = 100) -> None:
@@ -52,39 +52,85 @@ class Solution:
         with open(filename, 'r') as my_file:
             contents = my_file.read()
         lines = contents.split()
-        self.boss_hitpoints = int(lines[0].split(":")[1].strip())
-        self.boss_damage = int(lines[1].split(":")[1].strip())
-        self.boss_armor = int(lines[2].split(":")[1].strip())                                       
+        self.boss_hitpoints = int(lines[2].strip())
+        self.boss_damage = int(lines[4].strip())
+        self.boss_armor = int(lines[6].strip())
+        self.boss = Player(self.boss_hitpoints, self.boss_damage, self.boss_armor)
+
+    def _is_player_winner(self, player: Player) -> bool:
+        return player.get_turns_to_lose(self.boss) >= self.boss.get_turns_to_lose(player)                                      
+    
+    def _check_with_rings(self, damage: int, armor: int, cost: int, results: set[int], win: bool=True):
+        for i, (ring_damage, ring_armor, ring_cost) in enumerate(rings):
+            player = Player(damage = damage + ring_damage, armor = armor + ring_armor)
+            if (win and self._is_player_winner(player)) or (not win and not self._is_player_winner(player)):
+                results.add(cost + ring_cost)
+            for ring_2_damage, ring_2_armor, ring_2_cost in rings[i+1:]:
+                player = Player(damage = damage + ring_damage + ring_2_damage, armor = armor + ring_armor + ring_2_armor)
+                if (win and self._is_player_winner(player)) or (not win and not self._is_player_winner(player)):
+                    results.add(cost + ring_cost + ring_2_cost)
 
     @time_it
-    def solution_1(self, steps: int) -> int:
+    def solution_1(self) -> int:
         '''
         get result for task 1
         '''
-        result = 0
-        return result
+        results = set()
+        for damage, cost in damage_gains_vs_costs.items():
+            # case 1 - player only owns weapon
+            player = Player(damage=damage)
+            if self._is_player_winner(player):
+                results.add(cost)
+            # case 2 - weapon + ring(s)
+            self._check_with_rings(damage, 0, cost, results)
+            for armor, armor_cost in armor_gains_vs_costs.items():
+                # case 3 - weapon + armor
+                player = Player(damage=damage, armor=armor)
+                if self._is_player_winner(player):
+                    results.add(cost + armor_cost)
+                # weapon + armor + ring(s)
+                self._check_with_rings(damage, armor, cost + armor_cost, results)
+
+
+        return min(results) if results else -1
 
 
     @time_it
-    def solution_2(self, steps: int) -> int:
+    def solution_2(self) -> int:
         '''
         get result for task 2
         '''
-        result = 0
-        return result
+        results = set()
+        for damage, cost in damage_gains_vs_costs.items():
+            # case 1 - player only owns weapon
+            player = Player(damage=damage)
+            if not self._is_player_winner(player):
+                results.add(cost)
+            # case 2 - weapon + ring(s)
+            self._check_with_rings(damage, 0, cost, results, False)
+            for armor, armor_cost in armor_gains_vs_costs.items():
+                # case 3 - weapon + armor
+                player = Player(damage=damage, armor=armor)
+                if not self._is_player_winner(player):
+                    results.add(cost + armor_cost)
+                # weapon + armor + ring(s)
+                self._check_with_rings(damage, armor, cost + armor_cost, results, False)
+
+
+        return max(results) if results else -1
 
 
 def main():
     print('TEST 1')
     sol = Solution('2015/Day_21/test.txt')
     print('TEST 1')
-    print('test 1:', sol.solution_1(4), 'should equal 4')
-    print('test 2:', sol.solution_2(5), 'should equal 17')
+    print('test 1:', sol.solution_1(), 'should equal 4')
+    print('test 2:', sol.solution_2(), 'should equal 17')
     print('SOLUTION')
     sol = Solution('2015/Day_21/task.txt')
     print('SOLUTION')
-    print('Solution 1:', sol.solution_1(100))
-    print('Solution 2:', sol.solution_2(100))
+    print('Solution 1:', sol.solution_1())
+    print('Solution 2:', sol.solution_2())
    
 
 
